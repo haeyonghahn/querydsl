4b4483d01c23855e1d6dc755b8dc3be9a371d8f3
haeyonghahn <yong80211@gmail.com>
2022-08-08 00:53:11 +0900

    first commit

---
+++ src/test/java/study/querydsl/QuerydslApplicationTest.java
@@ -0,0 +1,36 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+public class QuerydslApplicationTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h"); // QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertEquals(result, hello);
+        Assertions.assertEquals(result.getId(), hello.getId());
+    }
+}
